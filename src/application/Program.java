7af035db8b1a797c160b44dbb4b5128e088703bf
4Miguel2 <delgdevoleper@gmail.com>
2023-12-06 11:38:18 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import bordgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        
+        Position p = new Position(2, 3);
+        System.out.println(p);
     }}
